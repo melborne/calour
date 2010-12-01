a1d1842b417a893412a5003a7088ea575d60d9bf
kyoendo <postagie@gmail.com>
2010-12-01 21:27:22 +0900

    rewrite colorize_calendar add holiday coloring using Google Calendar API

--- lib/calour.rb
+++ lib/calour.rb
@@ -1,35 +1,37 @@
 #-*-encoding: utf-8-*-
 require 'tempfile'
 require 'termcolor'
-autoload :Date, 'date'
+require "open-uri"
+require "nokogiri"
 
 class Calour
-  WD = %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+  WD = %w(January February March April May June July August September October November December)
   attr_accessor :colors
 
   def initialize(opts={})
-    @colors = {title: [:green,:yellow], today: :green, saturday: :cyan, sunday: :magenta}
+    @colors = {title: [:green,:yellow], today: :green, saturday: :cyan, sunday: :magenta, :holiday => :red}
     opts.keep_if { |k, v| colors.keys.include? k }
     colors.update(opts)
   end
 
   def cal(*args)
-    @mon, @year = parse_argument(args)
-    rdtout, rdterr = redirect_system_out { "cal #{@mon} #{@year}" }
+    @month, @year = parse_argument(args)
+    rdtout, rdterr = redirect_system_out { "cal #{@month} #{@year}" }
     @calendar, err = [rdtout, rdterr].map { |io| io.open.read }
     raise ArgumentError, err unless err.empty?
     colorize_calendar
   ensure
     [rdtout, rdterr].map { |io| io.close if io }
   end
-
   
   private
   def parse_argument(args)
     mon, year = args.sort
-    if !mon.nil? && year.nil?
+    if mon.nil? && year.nil?
+      mon, year = Time.now.mon, Time.now.year
+    elsif year.nil?
       if mon >= 100
-        year, mon = mon, year
+        mon, year = year, mon
       else
         year = Time.now.year
       end
@@ -48,82 +50,159 @@ class Calour
   end
 
   def colorize_calendar
-    colorize_weekend(:sunday => 0, :saturday => 18) # this colorize must be first.
-    colorize_today
-    colorize_title
-    colorize_holiday
+    @calendar =
+      @calendar.lines.inject("") do |mem, line|
+        line =
+          case line
+          when title? then colorize_title(line, *colors[:title])
+          when year?  then colorize_year[line, colors[:title][1]]
+          when dates? then colorize_weekend(line, :sunday => 0, :saturday => 18)
+          else line
+          end
+        mem << line
+      end
+    colorize_specific_days(today)
+    colorize_specific_days(holidays)
     @calendar.termcolor
   end
 
-  def colorize_title
-    wd, year = colors[:title]
-    @calendar =
-      @calendar.lines.inject("") do |cal, line|
-        if title_line?(line) || year_line?(line)
-          line.gsub!(/(#{WD.join("|")})\w*(?=\b)/) { "<#{wd}>#{$&}</#{wd}>" }
-          line.gsub!(/\d{4}/) { "<#{year}>#{$&}</#{year}>"}
-        end
-        cal << line
-      end
+  def title?
+    ->l{ l =~ /#{WD.join("|")}/ }
+  end
+  
+  def year?
+    ->l{ l =~ /\d{4}/ }
   end
 
-  def title_line?(line)
-    line =~ /#{WD.join("|")}/
+  def dates?
+    ->l{ l =~ /^\s*\d{1,2}\D/ }
   end
-  
-  def year_line?(line)
-    line =~ /\b\d{4}\b/
+
+  def colorize_title(line, wd, year)
+    line = line.gsub(/(#{WD.join("|")})/) { "<#{wd}>#{$&}</#{wd}>" }
+    colorize_year[line, year]
   end
-  
-  def colorize_weekend(opts)
-    unit_width = 22
+
+  def colorize_year
+    ->line, year { line.gsub(/\d{4}/) { "<#{year}>#{$&}</#{year}>"} }
+  end
+
+  def colorize_weekend(line, opts)
     opts = opts.sort_by { |_, v| -v }
-    @calendar =
-      @calendar.lines.inject("") do |cal, line|
-        unless title_line?(line) || line =~ /^\n$/
-          2.downto(0) { |i|
-            opts.each do |wd, offset|
-              pos = unit_width*i+offset
-              (line[pos, 2] = \
-                  "<#{colors[wd]}>#{line[pos, 2]}</#{colors[wd]}>") rescue nil
-            end
-          }
-        end
-        cal << line
-      end
+    2.downto(0) do |i|
+      opts.each { |wd, offset|
+        pos = UNIT_WIDTH()*i+offset
+        (line[pos, 2] = "<#{colors[wd]}>#{line[pos, 2]}</#{colors[wd]}>") rescue nil
+      }
+    end
+    line
   end
 
-  def colorize_today
-    t = Time.now
-    if [/#{t.year}/, /#{t.strftime("%b")}/].all? { |e| @calendar =~ e }
-      n = @year ? count_nth(t) : 1
-      pos = -1
-      n.times { pos = @calendar.index(/#{t.day}/, pos+1) }
-      @calendar[pos, 2] = "<on_#{colors[:today]}>#{t.day}</on_#{colors[:today]}>"
+  def UNIT_WIDTH
+    22
+  end
+
+  def today
+    { Time.now => :today }
+  end
+  
+  def holidays
+    parse_xml_to_hash(get_holidays)
+  end
+
+  def colorize_specific_days(targets)
+    color, on = set_color_mode(targets)
+
+    if @month.nil?
+      targets.each do |date, name|
+        mon = WD[date.mon-1]
+        start, side = false, nil
+        @calendar =
+          @calendar.lines.inject("") do |mem, line|
+            if mpos = line.index(mon) # set a position of target month title
+              side = detect_target_column(mpos, line) # select target column from mpos
+              start = true
+            end
+            
+            # find a target date position in the target column
+            # for lines after month title
+            if start
+              left, right = calc_range(side, line)
+              pos = line.index(/(?<=\D)#{date.day}\D/, left)
+              if pos && pos < right
+                line[pos, date.day.to_s.size] = 
+                          "<#{on}#{color}>#{date.day}</#{on}#{color}>"
+                start, side = false, nil
+              end
+            end
+            mem << line
+          end
+      end
+    else
+      targets.each do |date, name|
+        next unless date.mon == @month
+        day = date.day
+        pos = @calendar.index(/(?<=\D)#{day}\D/)
+        @calendar[pos, day.to_s.size] =
+            "<#{on}#{color}>#{day}</#{on}#{color}>"
+      end
     end
     @calendar
   end
 
-  # calculate position of the target date in a yearly calendar
-  def count_nth(date)
-    mindex = ->obj{ obj.index(date.mon).next  }
-    case date.day
-    when 31
-      mindex.call [1,3,6,8,12]
-    when 30
-      mindex.call (1..12).to_a - [2]
-    when 29
-      Date.new(date.year).leap? ? mindex.call((1..12).to_a - [2]) : date.mon
+  def set_color_mode(targets)
+    if targets.values.include?(:today)
+      [colors[:today], 'on_']
     else
-      date.mon
+      colors[:holiday]
+    end
+  end
+
+  def detect_target_column(pos, line)
+    unit = line.size / 3
+    case pos
+    when 0...unit then return :left
+    when unit...unit*2 then return :center
+    else return :right
     end
   end
   
-  @@url = "http://www.google.com/calendar/feeds/
-                japanese@holiday.calendar.google.com/public/full?"
+  def calc_range(side, line)
+    unit = line.size / 3
+    case side
+    when :left then return 0, unit
+    when :center then return unit, unit*2
+    else return unit*2, unit*3
+    end
+  end
 
-  def colorize_holiday
-    
+  # use Google Calendar Data API
+  def get_holidays #TODO: use local db for keeping data
+    open URL() + PARAM(*build_date_range)
+  end
+
+  def URL
+    "http://www.google.com/calendar/feeds" +
+                "/japanese@holiday.calendar.google.com/public/full?"
+  end
+
+  def PARAM(sd, ed)
+    "start-min=#{sd}&start-max=#{ed}"
+  end
+
+  def build_date_range
+    st, ed = @month ? [format("%02d", @month)] * 2 : ["01", "12"]
+    return "#{@year}-#{st}-01", "#{@year}-#{ed}-31"
+  end
+
+  def parse_xml_to_hash(xml)
+    data = Nokogiri::HTML(xml)
+    h = {}
+    data.css("entry").each do |node|
+      date = node.css("gd", "when").attr("starttime").value
+      h[Time.parse date] = node.css("title").text
+    end
+    h
   end
 end
 
--- test/tc_calour.rb
+++ test/tc_calour.rb
@@ -13,44 +13,55 @@ class TestCal < Test::Unit::TestCase
     assert_match(/Oct.*2012/, @c.cal(10, 2012))
     assert_match(/Feb.*1980/, @c.cal(1980, 2))
     assert_match(/Jul.*2200/, @c.cal(2200, 07))
-    assert_match(/Nov.*2010/, @c.cal())
+    assert_match(/Dec.*2010/, @c.cal())
     assert_match(/May.*2010/, @c.cal(5))
     assert_match(/1800/, @c.cal(1800))
     assert_match(/2010/, @c.cal(2010))
   end
-
+  
   def test_argument_error
     assert_raise(ArgumentError) { @c.cal(45) }
     assert_raise(ArgumentError) { @c.cal(13, 2000) }
     assert_raise(ArgumentError) { @c.cal(:hello) }
   end
-
+  
   def test_initialize_opts
     c = Calour.new(:hello => :orange, :title => :blue)
-    expected = {:title=>:blue, :today=>:green, :saturday=>:cyan, :sunday=>:magenta}
+    expected = {:title=>:blue, :today=>:green, :saturday=>:cyan, :sunday=>:magenta, :holiday => :red}
     assert_equal(expected, c.colors)
   end
-
+  
   def test_color_today
     c = Calour.new(:today => :blue)
     assert_match(/\e\[42m#{@t.day}\e\[0m/, @c.cal)
     assert_match(/\e\[44m#{@t.day}\e\[0m/, c.cal)
   end
-
+  
   def test_color_weekend
-    sat = ((6 - @t.wday) + @t.day)%7
-    sun = ((0 - @t.wday) + @t.day)%7 + 7
+    sat = @t.day % 7 + (6 - @t.wday)
+    sun = @t.day % 7 + (7 - @t.wday)
     assert_match(/\e\[36m #{sat}\e\[0m/, @c.cal)
     assert_match(/\e\[35m #{sun}\e\[0m/, @c.cal)
   end
-
+  
   def test_color_title
     month = @t.strftime("%B")
     assert_match(/\e\[32m#{month}\e\[0m/, @c.cal)
     assert_match(/\e\[32mAugust\e\[0m/, @c.cal(8, 2000))
   end
 
-  def test_color_holiday
-    assert_match(/\e\[31m23\e\[0m/, @c.cal(12))
+  def test_color_specific_days_for_monthly_calendar
+    today = Time.now
+    holidays = [[1, 1], [1, 10], [2, 11], [3, 21], [4, 29], [5, 3], [5, 4], [5, 5], [7, 18], [9, 19], [9, 23], [10, 10], [11, 3], [11, 23], [12, 23]]
+    assert_match(/\e\[42m#{today.day}\e\[0m/, @c.cal)
+    holidays.each do |mon, day|
+      assert_match(/\e\[31m#{day}\e\[0m/, @c.cal(mon, 2011))
+    end
+  end
+
+  def test_color_specific_days_for_yearly_calendar
+    today = Time.now
+    holidays = [[1, 1], [1, 10], [2, 11], [3, 21], [4, 29], [5, 3], [5, 4], [5, 5], [7, 18], [9, 19], [9, 23], [10, 10], [11, 3], [11, 23], [12, 23]]
+    puts @c.cal(2011)
   end
 end
